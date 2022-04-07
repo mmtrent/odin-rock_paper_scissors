a8d6c4e89501c3ea5e3cc77c447c6fc05708f88e
mmtrent <mmtrent@gmail.com>
2022-04-06 21:28:09 -0700

    Add more commenting to rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,6 @@
 function computerPlay() { // return either Rock, Paper, or Scissors randomly
-    // create array of three strings: Rock, Paper, and Scissors
-    const playArr = [
+    
+    const playArr = [ // create array of three strings: Rock, Paper, and Scissors
         'Rock',
         'Paper',
         'Scissors'
@@ -12,30 +12,28 @@ function computerPlay() { // return either Rock, Paper, or Scissors randomly
     return playArr[randomNumber];
 }
 
-let playerInput;
+let playerInput; 
 let compScore;
 let playerScore;
 
-function capitalizeFirstLetter(string) {
+function capitalizeFirstLetter(string) { // ensure string is formatted with first letter capitalized and the rest of the letters lower case
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-let playerSelection;
-let computerSelection;
+let playerSelection; // variable to store the formatted player move selection
+let computerSelection; // variable to store the computers move from the computerPlay function
 
 
 function playRound() { // take two parameters (playerSelection and computerSelection) and return string declaring winner of round
-    // logic to decide which play beats other plays
+    computerSelection = computerPlay(); // run computerPlay function and store the returned result in variable
+    playerInput = prompt("Rock, Paper, or Scissors?"); // prompt user for their move selection
+    playerSelection = capitalizeFirstLetter(playerInput); // format the user input
 
-    computerSelection = computerPlay();
-    playerInput = prompt("Rock, Paper, or Scissors?");
-    playerSelection = capitalizeFirstLetter(playerInput);
-
-    if (playerSelection === computerSelection)
+    if (playerSelection === computerSelection) // if player and computer have the same move, return a tie and don't increment either score
     {
         return "Tie!";
     }
-    else if (playerSelection === "Rock") {
+    else if (playerSelection === "Rock") { // if player selects rock, check against game logic and increment the appropriate score
         if (computerSelection === "Scissors") {
             playerScore ++;
             return "You Win. Rock beats Scissors";
@@ -65,12 +63,12 @@ function playRound() { // take two parameters (playerSelection and computerSelec
 }
 
 
-function game() {
+function game() { // function to call the playRound function i number of times, increment player and computer scores, decide the winner and print to console
     compScore = 0;
     playerScore = 0;
 
     for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
+        console.log(playRound(playerSelection, computerSelection)); // play 5 rounds of Rock, Paper, Scissors
     }
 
     console.log("Your score is: " + playerScore);
@@ -87,4 +85,4 @@ function game() {
     }
 }
 
-game();
+game(); // call game function
